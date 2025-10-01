a90613a5d5e99861fe6521ef22187e5d6c4d5890
Kelly J <[kelly4410@gmail.com]>
2025-10-01 16:08:53 -0600

    cookie check for search route

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,26 @@
 // React imports
 import React from "react";
+//
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 // React bootstrap imports
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 // Additional Components
 import SearchComponent from "../components/auth_components/SearchComponent";
 
-export default function SearchPage() {
+export default async function SearchPage() {
+    // Get cookies from the request
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+
+    // If no token is found, redirect to the login page
+    if (!token) {
+        redirect('/login');
+    }
+
+    // TODO: check if token is valid
+
     return (
         <div>
             <h1>Search Page</h1>
